2887e3a0cc4f377ebd0532680422019ab5490ea8
Md. Nazmul Huda <me@nazmul-huda.com>
2023-12-03 23:41:28 +0600

    framework installed

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Logo from '@/components/logo'
+
+export default function Home() {
+  return (
+    <main>
+      <Logo />
+      <p className='text-center'>Coming soon...</p>
+    </main>
+  )
+}
