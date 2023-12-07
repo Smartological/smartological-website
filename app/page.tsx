5a6a538a3b10544322f150c338af91e1a04ccc9a
Md. Nazmul Huda <me@nazmul-huda.com>
2023-12-08 00:44:42 +0600

    added shadcn-ui library

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Logo from '@/components/logo'
+import Logo from './components/logo'
 
 export default function Home() {
   return (
     <>
-      <main className='h-screen w-screen bg-[#42599a] text-white text-3xl font-light flex flex-col items-center justify-center gap-1'>
+      <main className='h-screen w-screen bg-[#42599a] text-white flex flex-col items-center justify-center gap-1'>
         <Logo width={500} />
         <p>We&apos;re coming soon!!</p>
       </main>
